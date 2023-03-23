be28351444181506dc581f35bcf3aa007ddb0ebd
fh9g12 <fintanhealy@live.co.uk>
2023-03-23 21:19:10 +0000

    removed modelnamespace

--- tbx/+baff/+model/@Beam/TemplateHdf5.m
+++
@@ -1,5 +0,0 @@
-function TemplateHdf5(filepath,loc)
-    baff.model.Element.TemplateHdf5(filepath,loc);
-    baff.model.station.Beam.TemplateHdf5(filepath,loc);
-end
-
--- tbx/+baff/+model/@BluffBody/TemplateHdf5.m
+++
@@ -1,5 +0,0 @@
-function TemplateHdf5(filepath,loc)
-    baff.model.Element.TemplateHdf5(filepath,loc);
-    baff.model.station.Body.TemplateHdf5(filepath,loc);
-end
-
