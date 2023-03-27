312d04799b1f2f413bb1155a48f1adb2a87ac860
fh9g12 <fintanhealy@live.co.uk>
2023-03-27 11:33:57 +0100

    add static method to distribute mass along a wing distribution is a function on area

---
+++ tbx/+baff/@Wing/DistributeMass.m
@@ -0,0 +1,25 @@
+function obj = DistributeMass(obj, mass, Nele,opts)
+    arguments
+        obj
+        mass
+        Nele
+        opts.BeamOffset = 0;
+        opts.tag = 'wing_mass';
+    end
+    % create N lumped masses spread across the wing with the fraction at each
+    % point proportional to the chord at each point
+    etas = linspace(obj.AeroStations(1).Eta,obj.AeroStations(end).Eta,Nele+1);
+    secs = obj.AeroStations.interpolate(etas);
+    NormAreas = secs.GetNormAreas();
+    masses = NormAreas./sum(NormAreas) * mass;
+    % get postions of the masses
+    etas = linspace(obj.AeroStations(1).Eta,obj.AeroStations(end).Eta,Nele+2);
+    etas = etas(2:end-1);
+    secs = obj.AeroStations.interpolate(etas);
+    %create the point masses and add to the wing
+    for i = 1:Nele
+        tmp_mass = baff.Mass(masses(i),"eta",etas(i),"Name",sprintf('%s_%.0f',opts.tag,i));
+        tmp_mass.Offset = [opts.BeamOffset*secs(i).Chord;0;0];
+        obj.add(tmp_mass);
+    end
+end
